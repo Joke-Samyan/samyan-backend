c6e375dd443d9a2e0d3f359d44a2e540db10b946
Supawich Sitdhipol <supawichs@gmail.com>
2022-10-24 22:17:37 +0700

    refactor: fix and refactor to utils function

--- prelabel/publisher.py
+++ prelabel/publisher.py
@@ -2,15 +2,14 @@ import pika
 import cv2
 import json
 import base64
+from utils import encode_jpg_from_path
 
 
 def send_to_image_classifier_queue(image_path="./demo/dog_demo.jpeg", file_name="dog_demo.jpeg"):
     """
     Send an image to the image classifier queue
     """
-    image = cv2.imread(image_path)
-    encoded_image = base64.b64encode(cv2.imencode(".jpg", image)[1]).decode()
-
+    encoded_image = encode_jpg_from_path(image_path)
     connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
     channel = connection.channel()
 
@@ -51,7 +50,7 @@ def send_to_ocr_queue(image_path="./demo/dog_demo.jpeg", file_name="dog_demo.jpe
 
 def main():
     send_to_image_classifier_queue()
-    send_to_ocr_queue
+    send_to_ocr_queue()
 
 
 if __name__ == "__main__":
